83dca8f9cab85ad2688397b8549d774b9d9d1ebb
Spinny03 <fili.spin2003@gmail.com>
2022-04-10 21:54:47 +0200

    aggiunte icone e sql

---
+++ table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE LDI(
+    `id` INTEGER PRIMARY KEY,
+    `name` varchar(255),
+    `description` varchar(255),
+    `registrationDate` timestamp,
+    `type` varchar(255),
+    `value` varchar(255)
+);
+
+CREATE TABLE username(
+    `email` varchar(255) PRIMARY KEY,
+    `firstName` varchar(255),
+    `surname` varchar(255),
+    `registrationDate` timestamp,
+    `notice` TINYINT(1),
+    `pasw` varchar(255) NOT NULL
+);
