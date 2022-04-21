6904add7e6d033dda597193a24066152b1e11123
Alessio Barletta <barle.ale@gmail.com>
2022-04-21 14:14:23 +0200

    navBar_Sample
    navabar

---
+++ navBar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/navBar.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="divWrapper">
+        <a href="#" class="camera"><img src="img/icon/camButton.svg" alt=""></a>
+        <nav class="bottomNav">
+            <div class="insideNav">
+            <img src="img/icon/searchOff.svg" alt="" class="icon">
+            <img src="img/icon/boardsOff.svg" alt="" class="icon">
+            </div>
+            <div class="insideNav">
+            <img src="img/icon/preferOff.svg" alt="" class="icon">
+            <img src="img/icon/profileOff.svg" alt="" class="icon">
+            </div>
+        </nav>
+    </div>
+    
+</body>
+</html>
