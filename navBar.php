528d0da7d70fae83cae654519ebfcdabc90c43c0
Alessio Barletta <barle.ale@gmail.com>
2022-04-22 09:40:12 +0200

    cards

--- navBar.php
+++ navBar.php
@@ -5,22 +5,60 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/navBar.css">
+    <link rel="stylesheet" href="css/cardsMenu.css">
+    <link rel="stylesheet" href="css/textFormat.css">
     <title>Document</title>
 </head>
 <body>
+
+    <div class="cardsContainer">
+        <div class="card">
+            <div class="imageGallery">
+
+            </div>
+            <div class="cardBottom">
+                <span class="cardTitle">paintings</span>
+            </div>
+        </div>
+        <div class="card">
+            <div class="imageGallery">
+
+            </div>
+            <div class="cardBottom">
+                <span class="cardTitle">OpereProva</span>
+            </div>
+        </div>
+        <div class="card">
+            <div class="imageGallery">
+
+            </div>
+            <div class="cardBottom">
+                <span class="cardTitle">OpereProva</span>
+            </div>
+        </div>
+        <div class="card">
+            <div class="imageGallery">
+
+            </div>
+            <div class="cardBottom">
+                <span class="cardTitle">OpereProva</span>
+            </div>
+        </div>
+    </div>
+
     <div class="divWrapper">
         <a href="#" class="camera"><img src="img/icon/camButton.svg" alt="" class="icon"></a>
         <nav class="bottomNav">
             <div class="insideNav">
-            <img src="img/icon/searchOff.svg" alt="" class="icon">
-            <img src="img/icon/boardsOff.svg" alt="" class="icon">
+            <a href="#"><img src="img/icon/searchOff.svg" alt="" class="icon"></a>
+            <a href="#"><img src="img/icon/boardsOff.svg" alt="" class="icon"></a>
             </div>
             <div class="insideNav">
-            <img src="img/icon/preferOff.svg" alt="" class="icon">
-            <img src="img/icon/profileOff.svg" alt="" class="icon">
+            <a href="#"><img src="img/icon/preferOff.svg" alt="" class="icon"></a>
+            <a href="#"><img src="img/icon/profileOff.svg" alt="" class="icon"></a>
             </div>
         </nav>
     </div>
-    
+
 </body>
 </html>
