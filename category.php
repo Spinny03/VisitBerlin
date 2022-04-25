ef051823de905aa6b4bfe63d9dcfffa3e18a947d
Spinny03 <fili.spin2003@gmail.com>
2022-04-25 13:52:58 +0200

    aggiunto cookie

--- category.php
+++ category.php
@@ -1,5 +1,10 @@
 <?php 
     session_start(); 
+    if(empty($_SESSION["user"])){
+        if(isset($_COOKIE["user"])){
+            $_SESSION["user"] = $_COOKIE["user"];
+        }
+    }
     $conn = new mysqli("localhost", "root", "");  
     if ($conn->connect_error){
         exit("Connessione fallita: " . $conn->connect_error);
