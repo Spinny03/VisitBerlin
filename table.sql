be70264084c6bb7954bd21d53a942794c5c34c03
Spinny03 <fili.spin2003@gmail.com>
2022-04-25 13:44:04 +0200

    luoghi berlino

--- table.sql
+++ table.sql
@@ -28,3 +28,32 @@ INSERT INTO `tipo`(`id`, `name`) VALUES (`3`,`edificio`);
 INSERT INTO `tipo`(`id`, `name`) VALUES (`4`,`statua`);
 INSERT INTO `tipo`(`id`, `name`) VALUES (`5`,`museo`);
 INSERT INTO `ldi`(`id`, `name`, `description`, `registrationDate`, `type`, `value`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6]);
+
+Pergamonmuseum
+Porta di Brandeburgo
+Topographie des Terrors
+Duomo di Berlino
+Wasserturm Prenzlauer Berg
+Monument to the Polish soldiers and German anti-fascists
+Memoriale agli omosessuali perseguitati sotto il nazismo
+Memoriale sovietico (Tiergarten)
+Memorial to the Victims of National Socialist 'Euthanasia' Killings
+Monument to the Polish soldiers and German anti-fascists
+Museo del Muro
+Muro di Berlino
+Checkpoint Charlie
+Palazzo del Reichstag
+Memorial to May 10, 1933 Nazi Book Burning
+Castello di Charlottenburg
+Kaiser-Wilhelm-Gedächtniskirche
+Skulptur "Berlin" Brigitte und Martin Matschinsky-Denninghoff
+The Yellow Man (Os Gemeos)
+Oberbaum Bridge
+Skulptur Galileo
+Haus der Kulturen der Welt
+Urania Weltzeituhr
+Brunnen der Völkerfreundschaft
+Fontana di Nettuno
+Marx-Engels-Forum
+Volksbühne am Rosa-Luxemburg-Platz
+
