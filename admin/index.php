8d2078b165a9ecf4fc6789b480f061692e4e904b
altassara <barle.ale@gmail.com>
2022-05-23 21:49:00 +0200

    pero se non committi

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Admin</title>
+</head>
+<body>
+    <div class="nav">
+        <h1>Pannello di controllo</h1>
+        <a href="#">esci</a>
+    </div>
+    <div class="flex">
+        <a href="editldi.php">Modifica luoghi di interesse</a>
+        <a href="editType.php">Modifica le categorie</a>
+    </div>
+</body>
+</html>
